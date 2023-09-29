dee4dc70f22eff7b8d53db6937931141bf4f9253
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2023-09-29 15:34:45 +0300

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,40 @@
-/** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
+// /** @type {import('next').NextConfig} */
+// const { i18n } = require('./next-i18next.config');
 
+// const nextConfig = {
+//   reactStrictMode: true,
+//   i18n,
+// }
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+      },
+      {
+        source: '/uploads/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/uploads/:path*`
+      }
+    ]
+  },
+  images: {
+    unoptimized: true,
+    domains: ["127.0.0.1", '45.93.138.46', 'localhost', 'www.api.localshop', "localshop", "api.localshop"]
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader']
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
