91a72ff53eb9b138aa14e7028548459ea1c86abd
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2023-10-09 22:09:53 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,42 +15,42 @@ import { getTheme } from '../../components/layout/Theme';
 type Props = {
   newProducts: ProductsResponseType
   popularProducts: ProductsResponseType
-  // heroBanner?: any
+  heroBanner?: any
 }
 
-export default function Home({ newProducts, popularProducts }: Props) {
+export default function Home({ newProducts, popularProducts, heroBanner }: Props) {
   const theme = getTheme()
 
-  // const renderNewProducts = newProducts.data.map((p) => (
-  //   <BasicCard product={p} key={p.id} />
-  // ))
+  const renderNewProducts = newProducts.data.map((p) => (
+    <BasicCard product={p} key={p.id} />
+  ))
   const renderPopularProducts = popularProducts.data.map((p) => (
     <BasicCard product={p} key={p.id} />
   ))
 
-  // const renderBanners = heroBanner?.data.attributes.banner.map((b: any) => {
-  //   return (
-  //     <Stack key={b.images.data[0].id} sx={{ position: 'relative', width: '100%', backgroundColor: theme.palette.secondary.main, height: { md: '50vh', xs: '70vh' }, }}>
-  //       <Stack position={'absolute'} sx={{ pt: 22, zIndex: 1, alignItems: { md: 'center', xs: 'flex-start' }, width: '100%', px: { sm: 4, xs: 2 }, justifyItems: 'flex-start' }}>
-  //         <Typography variant='h1' fontWeight={500} color={theme.palette.secondary.main}>
-  //           {b.title}
-  //         </Typography>
-  //         <Typography variant='subtitle1' color={theme.palette.secondary.main}>
-  //           {b.subtitle}
-  //         </Typography>
-  //         <Link passHref href={'/products'}>
-  //           <Button variant='contained' color='secondary' sx={{ mt: 4 }}>
-  //             View products
-  //           </Button>
-  //         </Link>
-  //       </Stack>
-  //       <Image src={b.images?.data[0]?.attributes.url ?? ''} alt={''} fill objectFit='cover' objectPosition='top' />
-  //     </Stack>
-  //   )
-  // })
+  const renderBanners = heroBanner?.data.attributes.banner.map((b: any) => {
+    return (
+      <Stack key={b.images.data[0].id} sx={{ position: 'relative', width: '100%', backgroundColor: theme.palette.secondary.main, height: { md: '50vh', xs: '70vh' }, }}>
+        <Stack position={'absolute'} sx={{ pt: 22, zIndex: 1, alignItems: { md: 'center', xs: 'flex-start' }, width: '100%', px: { sm: 4, xs: 2 }, justifyItems: 'flex-start' }}>
+          <Typography variant='h1' fontWeight={500} color={theme.palette.secondary.main}>
+            {b.title}
+          </Typography>
+          <Typography variant='subtitle1' color={theme.palette.secondary.main}>
+            {b.subtitle}
+          </Typography>
+          <Link passHref href={'/products'}>
+            <Button variant='contained' color='secondary' sx={{ mt: 4 }}>
+              View products
+            </Button>
+          </Link>
+        </Stack>
+        <Image src={b.images?.data[0]?.attributes.url ?? ''} alt={''} fill objectFit='cover' objectPosition='top' />
+      </Stack>
+    )
+  })
   return (
     <Layout>
-      {/* <AliceCarousel disableDotsControls disableButtonsControls items={renderBanners} /> */}
+      <AliceCarousel disableDotsControls disableButtonsControls items={renderBanners} />
       <Stack sx={{ maxWidth: 'lg', mx: 'auto', width: '100%', px: { sm: 4, xs: 2 }, pt: { sm: 4, xs: 2 } }}
         spacing={4}>
         {/* <PageTitle title='New products' /> */}
